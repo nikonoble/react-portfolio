1adbf3319841cb7781de72d48e380a41ec593e11
Niko Noble <nikonoble30@gmail.com>
2022-11-30 06:26:20 -1000

    Added auth page and styles

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
+import loginImg from "../../../static/assets/images/auth/login.jpg";
 
 export default class Auth extends Component {
     render() {
-        return <div>Auth...</div>;
+        return (
+            <div className="auth-page-wrapper">
+                <div 
+                className="left-column"
+                style={{
+                    backgroundImage: `url(${loginImg})`
+                }}
+                />
+
+                
+                
+                <div className="right-column">
+                    <h1>Login Component Goes Here</h1>
+                </div>
+            </div>
+        )
     }
 }
