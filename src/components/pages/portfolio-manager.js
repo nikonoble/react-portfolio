36f1c9e495849c7ab679a653cefdbea0b7f43d9b
Niko Noble <nikonoble30@gmail.com>
2022-12-05 08:45:21 -1000

    Added Dropzone components and form styles

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
     constructor() {
@@ -9,11 +10,26 @@ export default class PortfolioManager extends Component {
 
         this.state = {
             portfolioItems: []
-        }
+        };
+
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+
+    }
+
+
+    handleSuccessfulFormSubmission(portfolioItem) {
+        this.setState({
+            portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
+        });
+    }
+
+    handleFormSubmissionError(error) {
+        console.log("handleFormSubmissionError error", error);
     }
 
     getPortfolioItems() {
-        axios.get("https://nikonoble.devcamp.space/portfolio/portfolio_items", { 
+        axios.get("https://nikonoble.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc", { 
           withCredentials: true
         }).then(response => {
           this.setState({
@@ -33,7 +49,10 @@ export default class PortfolioManager extends Component {
         return (
             <div className='portfolio-manager-wrapper'>
                 <div className="left-column">
-                    <h1>Portfolio form...</h1>
+                    <PortfolioForm 
+                        handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+                        handleFormSubmissionError={this.handleFormSubmissionError}
+                    />
                 </div>
 
                 <div className="right-column">
