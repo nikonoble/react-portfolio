210b3b88dc23915cf8f53ff7a046ba5f37a23e7b
Niko Noble <nikonoble30@gmail.com>
2022-12-05 13:04:59 -1000

    Added edit button w/ styles

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -9,11 +9,19 @@ export default class PortfolioManager extends Component {
         super();
 
         this.state = {
-            portfolioItems: []
+            portfolioItems: [],
+            portfolioToEdit: {}
         };
 
-        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
         this.handleDeleteClick = this.handleDeleteClick.bind(this);
+         this.handleEditClick = this.handleEditClick.bind(this);
+    }
+
+    handleEditClick(portfolioItem) {
+        this.setState({
+            portfolioToEdit: portfolioItem
+        });
     }
 
     handleDeleteClick(portfolioItem) {
@@ -77,7 +85,8 @@ export default class PortfolioManager extends Component {
                 <div className="right-column">
                     <PortfolioSidebarList 
                     handleDeleteClick={this.handleDeleteClick}
-                    data={this.state.portfolioItems} 
+                    data={this.state.portfolioItems}
+                    handleEditClick={this.handleEditClick}
                     />
                 </div>
 
